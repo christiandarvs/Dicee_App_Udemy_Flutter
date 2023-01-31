047f363c108c039673a8321c6e95b6d3da5e63d1
christiandarvs <christiandarvin4@gmail.com>
2023-01-31 22:58:42 +0800

    add roll dice button, generate random number func

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(const MyApp());
 
@@ -10,24 +11,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-          backgroundColor: Colors.red.shade200,
+          backgroundColor: const Color.fromARGB(255, 124, 147, 159),
           appBar: AppBar(
             title: const Text('Dice App'),
             centerTitle: true,
-            backgroundColor: Colors.red.shade300,
+            backgroundColor: const Color(0xff3F51B5),
           ),
           body: const SafeArea(child: DicePage())),
     );
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDieNumber = 1;
+  int leftDieNumber = 1;
+  void rollDice() {
+    leftDieNumber = Random().nextInt(6) + 1;
+    rightDieNumber = Random().nextInt(6) + 1;
+    debugPrint('$leftDieNumber');
+    debugPrint('$rightDieNumber');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('asd'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('images/dice$leftDieNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('images/dice$rightDieNumber.png'),
+                ),
+              ),
+            ],
+          ),
+          ElevatedButton(
+              onPressed: (() {
+                setState(() {
+                  rollDice();
+                });
+              }),
+              child: const Text('Roll Dice'))
+        ],
+      ),
     );
   }
 }
